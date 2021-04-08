7e6f65013095f856f50dea7d33c50a3a0e857283
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2021-04-08 16:35:00 +1000

    ad orig dataset

--- scripts/make_bcr_cdf.py
+++ scripts/make_bcr_cdf.py
@@ -1,15 +1,13 @@
+import matplotlib.font_manager
 from bcr_pipeline.post_processing.bcr_plotting.main_analysis_plots import \
     pack_plotting_data
 from bcr_pipeline.post_processing.bcr_plotting.make_ranking_stat_probability_plot import (
     _add_rank_pdf)
 from matplotlib import pyplot as plt
+from matplotlib import rc
 from matplotlib import rcParams
 from matplotlib.lines import Line2D
-from matplotlib.ticker import (MultipleLocator,
-                               FormatStrFormatter,
-                               AutoMinorLocator)
-import matplotlib.font_manager
-from matplotlib import rc
+from matplotlib.ticker import (FormatStrFormatter)
 
 # matplotlib.font_manager.findfont("font.sans-serif", rebuild_if_missing=False)
 all_fonts = matplotlib.font_manager.findSystemFonts()
@@ -44,20 +42,38 @@ plt.rcParams['ytick.major.width'] = 2.5
 plt.rcParams['xtick.top'] = True
 plt.rcParams['ytick.right'] = True
 
-
-
 BANK = "../data/template_bank.csv"
-INJECTION = "../data/chunk21_res/injection_res.csv"
-BACKGROUND = "../data/chunk21_res/background_res.csv"
-FOREGROUND = "../data/chunk21_res/foreground_res.csv"
+INJECTION_REW = "../data/chunk21_res/injection_res.csv"
+BACKGROUND_REW = "../data/chunk21_res/background_res.csv"
+FOREGROUND_REW = "../data/chunk21_res/foreground_res.csv"
+INJECTION_ORIG = "../data/chunk21_orig/injection.csv"
+BACKGROUND_ORIG = "../data/chunk21_orig/background.csv"
+FOREGROUND_ORIG = "../data/chunk21_orig/foreground.csv"
+INJECTION="INJECTION"
+BACKGROUND="BACKGROUND"
+FOREGROUND="FOREGROUND"
+
+DATA = dict(
+    orig=dict(
+        INJECTION=INJECTION_ORIG,
+        BACKGROUND=BACKGROUND_ORIG,
+        FOREGROUND=FOREGROUND_ORIG,
+    ),
+    reweighted=dict(
+        INJECTION=INJECTION_REW,
+        BACKGROUND=BACKGROUND_REW,
+        FOREGROUND=FOREGROUND_REW,
+    ),
+)
+
 CATALOG = "../data/catalog.csv"
 
 
-def plot():
+def plot(data_type='orig', add_legend=False):
     plot_data = pack_plotting_data(
-        foreground_csv=FOREGROUND,
-        background_csv=BACKGROUND,
-        injection_csv=INJECTION,
+        foreground_csv=DATA[data_type][FOREGROUND],
+        background_csv=DATA[data_type][BACKGROUND],
+        injection_csv=DATA[data_type][INJECTION],
         rank='lnBCR',
         alpha=1.0e-6,
         beta=1.0e-4,
@@ -68,7 +84,6 @@ def plot():
     ax_cdf.set_ylim(0, 1)
     ax_cdf.set_ylabel('Survival Probability', fontsize="x-large")
     ax_cdf.set_xlabel(r'ln $\rho_{\mathrm{BCR}}$', fontsize="x-large")
-
 
     for idx, x_in_q in plot_data['foreground']['df'].iterrows():
         c = 'orange'
@@ -90,31 +105,35 @@ def plot():
     # colors = ["#d8d8d8", "#daf0f8", "#0173b2", "#e9b457"]
     # custom_lines = [Line2D([0], [0], color=c, lw=4) for c in colors]
     # l = ax_cdf.legend(custom_lines, labels, **lg_kwargs)
+    bbox_extra_artists = []
+    if add_legend:
+        lg_kwargs = dict(fontsize="small", loc="lower right",
+                         facecolor='white', framealpha=0.2, handlelength=0.75,
+                         handletextpad=0.3, labelspacing=0.2, markerscale=2)
+        
+        l = ax_cdf.legend(
+            [Line2D([0], [0], color="tab:gray", linewidth=4),
+             Line2D([0], [0], color="skyblue", linewidth=4),
+             Line2D([0], [0], color="tab:orange", linewidth=4), ],
+            ["Background", "Simulated", "Foreground"],
+            **lg_kwargs
+        )
+        frame = l.get_frame()
+        frame.set_linewidth(0)
+        bbox_extra_artists.append(l)
 
-    lg_kwargs = dict(fontsize="small", loc="lower right",
-                     facecolor='white', framealpha=0.2, handlelength=0.75,  handletextpad=0.3, labelspacing=0.2, markerscale=2)
-
-    l = ax_cdf.legend(
-        [Line2D([0], [0], color="tab:gray", linewidth=4),
-         Line2D([0], [0], color="skyblue", linewidth=4),
-         Line2D([0], [0], color="tab:orange", linewidth=4),],
-        ["Background", "Simulated", "Foreground"],
-        **lg_kwargs
-    )
-    frame = l.get_frame()
-    frame.set_linewidth(0)
-
-    fname = '../images/bcr_cdf_smaller_legend.png'
+    fname = f'../images/{data_type}_bcr_cdf_smaller_legend.png'
 
     ax_cdf.yaxis.set_major_formatter(FormatStrFormatter('% 1.1f'))
-    fig.savefig(fname, bbox_extra_artists=[l], bbox_inches='tight',  transparent=True)
-    print(f"current font family: { plt.rcParams['font.family']}")
+    fig.savefig(fname, bbox_extra_artists=bbox_extra_artists, bbox_inches='tight', transparent=False)
+    print(f"current font family: {plt.rcParams['font.family']}")
     print(f"Avail fonts: {plt.rcParams['font.sans-serif']}")
     print("Saved fig")
 
 
 def main():
-    plot()
+    plot('orig')
+    plot('reweighted')
 
 
 if __name__ == "__main__":
